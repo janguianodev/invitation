f89282df157b705b7200c24813a9580cb7812c6f
janguianodev <josue.anguiano@clickittech.com.mx>
2024-12-17 17:05:29 -0600

    chore: 🩹 add message column in the guests table

--- app/(authenticated)/guests/interfaces/GuestInterface.ts
+++ app/(authenticated)/guests/interfaces/GuestInterface.ts
@@ -1,12 +1,12 @@
 export interface GuestI {
   id: string;
   name: string | null;
-  email?: string | null;
   phoneNumber: string | null;
   invitedPeople: number | null;
   confirmedPeople?: number | null;
   invitationId?: string | null;
   guestSlug?: string | null;
+  message: string | null;
   // guestPasses: {
   //   id: string;
   //   name: string;
