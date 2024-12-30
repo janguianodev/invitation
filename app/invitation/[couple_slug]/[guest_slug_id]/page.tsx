f58c87f285673cd05b9b949ad40eacb613351084
janguianodev <josue.anguiano@clickittech.com.mx>
2024-12-30 14:09:06 -0600

    chore: 🩹 fix countdown days and date on invitation table

--- app/invitation/[couple_slug]/[guest_slug_id]/page.tsx
+++ app/invitation/[couple_slug]/[guest_slug_id]/page.tsx
@@ -1,12 +1,37 @@
 import { getInvitationDataBySlug } from "@/actions";
 import { InvitationDataI } from "@/interfaces";
 import { Template1 } from "@/templates/Template1";
+import type { Metadata } from "next";
 import { notFound } from "next/navigation";
 
 interface Props {
   params: {
     couple_slug: string;
     guest_slug_id: string;
+  };
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const couple_slug = params.couple_slug;
+  const guest_slug_id = params.guest_slug_id;
+
+  const invitationData = await getInvitationDataBySlug(
+    couple_slug,
+    guest_slug_id
+  );
+
+  return {
+    title: `Boda ${invitationData?.couple?.partner1Name.split(" ")[0]} & ${
+      invitationData?.couple?.partner2Name.split(" ")[0]
+    }`,
+    description: invitationData?.welcomeMessage,
+    openGraph: {
+      title: `Boda ${invitationData?.couple?.partner1Name.split(" ")[0]} & ${
+        invitationData?.couple?.partner2Name.split(" ")[0]
+      }`,
+      description: invitationData?.welcomeMessage || "",
+      images: [{ url: invitationData?.brideImage || "" }],
+    },
   };
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
-import type { Metadata } from "next";
 import "./globals.css";
 import { robotoFont } from "@/fonts";
 import { Providers } from "@/components";
 import { SessionProvider } from "next-auth/react";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: {
-    template: "%s | Wedding Invitation",
+    template: "%s | Invitation",
     default: "Wedding Invitation",
   },
   description: "Electronic invitation for your wedding",
