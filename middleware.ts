0d8cf0d09c821b11bcdfd31bfaff26f96b7afd3c
janguianodev <josue.anguiano@clickittech.com.mx>
2024-12-30 23:29:13 -0600

    chore: :sparkles: add some logs

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,7 @@ import { getToken } from "next-auth/jwt";
 export default async function middleware(req: Request) {
   const { pathname } = new URL(req.url);
 
+  // Lista de rutas públicas
   const publicRoutes = [
     "/_next",
     "/static",
@@ -13,24 +14,32 @@ export default async function middleware(req: Request) {
     "/images",
   ];
 
+  console.log("Middleware invoked for pathname:", pathname);
+
+  // Verificar si la ruta es pública
   const isPublicRoute = publicRoutes.some((route) =>
     pathname.startsWith(route)
   );
 
   if (isPublicRoute) {
+    console.log("Public route accessed:", pathname);
     return NextResponse.next();
   }
 
   if (pathname === "/auth/sign-in") {
+    console.log("Sign-in route accessed");
     return NextResponse.next();
   }
 
+  // Obtener el token JWT
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
   if (!token) {
+    console.log("No token found, redirecting to /auth/sign-in");
     return NextResponse.redirect(new URL("/auth/sign-in", req.url));
   }
 
+  console.log("Token found, allowing access to:", pathname);
   return NextResponse.next();
 }
 
